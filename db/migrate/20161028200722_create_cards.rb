f8c75ee247b8becb5ea9db64f92b292cd4acba26
Shawn LaCombe <lacombeshawn@yahoo.com>
2016-10-29 11:26:52 -0500

    Initial Commit

---
+++ db/migrate/20161028200722_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.text :description
+      t.integer :quantity
+      t.references :deck, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
